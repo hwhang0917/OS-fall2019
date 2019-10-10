5e1a91b057aa07e6bd24053f61cd65e8ea0bf9eb
Mark Whang <hwhang0917@gmail.com>
2019-10-10 10:56:27 +0000

    Initial commit for program 2

---
+++ program2/p2hswhang.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/type.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main() { // main entry point of the program
+    
+    return 0;
+}
